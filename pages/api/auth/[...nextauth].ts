df887859c7420955f23ba999ab2e91c4b8d628d8
elroy <roy.elodie2@gmail.com>
2024-02-19 22:42:22 +0100

    fix: build

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,6 @@
 import { env } from '@/lib/env';
 import { prisma } from '@/lib/prisma';
-import { PrismaAdapter } from '@auth/prisma-adapter';
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth, { AuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 
