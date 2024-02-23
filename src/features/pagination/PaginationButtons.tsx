e6cd02853c695d427d58efdad7c3a508a54b6d60
elroy <roy.elodie2@gmail.com>
2024-02-23 21:39:09 +0100

    feat: improve ui with prefetch, replace url and no scroll to top

--- src/features/pagination/PaginationButtons.tsx
+++ src/features/pagination/PaginationButtons.tsx
@@ -1,12 +1,13 @@
 "use client";
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import {
   ChevronLeftIcon,
   ChevronRightIcon,
   DoubleArrowLeftIcon,
   DoubleArrowRightIcon,
 } from "@radix-ui/react-icons";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 type PaginationButtonsProps = {
   page: number;
@@ -19,52 +20,89 @@ const PaginationButtons = ({
   totalPage,
   baseUrl,
 }: PaginationButtonsProps) => {
-  const router = useRouter();
-
-  const changeUrlPage = (page: string) => {
+  const getUrlPage = (page: string) => {
     const searchParams = new URLSearchParams({ page: page });
-    const url = `${baseUrl}?${searchParams.toString()}`;
-    router.push(url);
+    return `${baseUrl}?${searchParams.toString()}`;
   };
-
   return (
     <div className="flex justify-end items-center space-x-6 lg:space-x-8">
       <div className="flex w-[100px] items-center justify-center text-sm font-medium">
         Page {page > totalPage ? totalPage : page} sur {totalPage}
       </div>
       <div className="flex items-center space-x-2">
-        <Button
-          variant="outline"
-          className="hidden h-8 w-8 p-0 lg:flex"
-          onClick={() => changeUrlPage(String(1))}
-          disabled={page === 1}
-        >
-          <DoubleArrowLeftIcon className="h-4 w-4" />
-        </Button>
-        <Button
-          variant="outline"
-          className="h-8 w-8 p-0"
-          onClick={() => changeUrlPage(String(page - 1))}
-          disabled={page === 1}
-        >
-          <ChevronLeftIcon className="h-4 w-4" />
-        </Button>
-        <Button
-          variant="outline"
-          className="h-8 w-8 p-0"
-          onClick={() => changeUrlPage(String(page + 1))}
-          disabled={page === totalPage}
-        >
-          <ChevronRightIcon className="h-4 w-4" />
-        </Button>
-        <Button
-          variant="outline"
-          className="hidden h-8 w-8 p-0 lg:flex"
-          onClick={() => changeUrlPage(String(totalPage))}
-          disabled={page === totalPage}
-        >
-          <DoubleArrowRightIcon className="h-4 w-4" />
-        </Button>
+        {page === 1 ? (
+          <Button className="h-8 w-8 p-0" disabled>
+            <DoubleArrowLeftIcon className="h-4 w-4" />
+          </Button>
+        ) : (
+          <Link
+            href={getUrlPage(String(1))}
+            className={cn(
+              buttonVariants({ variant: "outline" }),
+              "h-8 w-8 p-0",
+              page === 1 ? "pointer-events-none" : null
+            )}
+            replace
+            prefetch
+            scroll={false}
+          >
+            <DoubleArrowLeftIcon className="h-4 w-4" />
+          </Link>
+        )}
+        {page === 1 ? (
+          <Button className="h-8 w-8 p-0" disabled>
+            <ChevronLeftIcon className="h-4 w-4" />
+          </Button>
+        ) : (
+          <Link
+            href={getUrlPage(String(page - 1))}
+            className={cn(
+              buttonVariants({ variant: "outline" }),
+              "h-8 w-8 p-0"
+            )}
+            replace
+            prefetch
+            scroll={false}
+          >
+            <ChevronLeftIcon className="h-4 w-4" />
+          </Link>
+        )}
+        {page === totalPage ? (
+          <Button className="h-8 w-8 p-0" disabled>
+            <ChevronRightIcon className="h-4 w-4" />
+          </Button>
+        ) : (
+          <Link
+            href={getUrlPage(String(page + 1))}
+            className={cn(
+              buttonVariants({ variant: "outline" }),
+              "h-8 w-8 p-0"
+            )}
+            replace
+            prefetch
+            scroll={false}
+          >
+            <ChevronRightIcon className="h-4 w-4" />
+          </Link>
+        )}
+        {page === totalPage ? (
+          <Button className="h-8 w-8 p-0" disabled>
+            <DoubleArrowRightIcon className="h-4 w-4" />
+          </Button>
+        ) : (
+          <Link
+            href={getUrlPage(String(totalPage))}
+            className={cn(
+              buttonVariants({ variant: "outline" }),
+              "h-8 w-8 p-0"
+            )}
+            replace
+            prefetch
+            scroll={false}
+          >
+            <DoubleArrowRightIcon className="h-4 w-4" />
+          </Link>
+        )}
       </div>
     </div>
   );
